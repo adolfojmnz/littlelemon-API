77dc22e9ca279976e65a52702aaef3ab43bb4542
Adolfo Jimenez <adolfoj@protonmail.com>
2023-01-30 09:53:01 -0400

    Add function-based views for learning porpuses

--- littlelemon/views.py
+++ littlelemon/views.py
@@ -24,3 +24,23 @@ class CategoryListView(ListCreateAPIView):
 class CategoryItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+# function-based views
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(['GET', 'POST'])
+def menuitem_list(request):
+    if request.method == 'GET':
+        queryset = MenuItem.objects.select_related('category').all()
+        serialized_data = MenuItemSerializer(queryset, many=True, context={'request': request})
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
+    
+    if request.method == 'POST':
+        serialized_data = MenuItemSerializer(data=request.data)
+        serialized_data.is_valid(raise_exception=True)
+        serialized_data.save()
+        return Response(serialized_data.data, status=status.HTTP_201_CREATED)
