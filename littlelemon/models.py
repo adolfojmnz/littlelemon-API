88663bdb499c4b780baa29ef618bf4955b1f9871
Adolfo Jimenez <adolfoj@protonmail.com>
2023-01-30 09:47:20 -0400

    Set default=0 in 'category' ForeignKeyField

--- littlelemon/models.py
+++ littlelemon/models.py
@@ -5,7 +5,7 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.SmallIntegerField()
-    category = models.ForeignKey('littlelemon.Category', on_delete=models.CASCADE, blank=True, null=True)
+    category = models.ForeignKey('littlelemon.Category', on_delete=models.CASCADE, default=0)
 
     def __str__(self):
         return self.title
