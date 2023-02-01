beca017aa1af31216ad8c577309a4675dd6d7770
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-01 08:32:40 -0400

    Add functionality for data validation and sanitization

--- littlelemon/serializers.py
+++ littlelemon/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
 from .models import MenuItem, Category
 
 from decimal import Decimal
+import bleach
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -11,12 +14,27 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'slug']
 
 
-class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
+class MenuItemSerializer(serializers.ModelSerializer):
     after_tax = serializers.SerializerMethodField(method_name='price_after_tax')
+    category = serializers.HyperlinkedIdentityField(view_name='category-detail', read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = MenuItem
-        fields = ['id', 'title', 'inventory', 'category', 'price', 'after_tax']
+        fields = ['id', 'title', 'inventory', 'category', 'category_id', 'price', 'after_tax']
+        extra_kwargs = {
+            'title': {
+                'validators': [
+                    UniqueValidator(queryset=MenuItem.objects.all()),
+                ],
+            },
+        }
  
     def price_after_tax(self, product:MenuItem):
         return round(product.price * Decimal(1.1), 2)
+    
+    def validate(self, attrs):
+        attrs['title'] = bleach.clean(attrs['title'])
+        if attrs['price'] < 2: raise serializers.ValidationError('The price should be greater or equal than 2')
+        if attrs['inventory'] < 0: raise serializers.ValidationError('The number of items in inventoy should be greater than 0')
+        return super().validate(attrs)
