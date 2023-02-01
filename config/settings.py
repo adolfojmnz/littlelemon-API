64d79936039c7c7ab69848df20500c51ade73600
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-01 09:42:02 -0400

    Add default pagination class and default items per page

--- config/settings.py
+++ config/settings.py
@@ -129,4 +129,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
         'rest_framework.filters.SearchFilter',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5
 }
