87079b3cff68f5e71421b1785c01164d27c843f2
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-01 08:36:12 -0400

    Add pagination and limit user per_page requests to 5

--- littlelemon/views.py
+++ littlelemon/views.py
@@ -20,11 +20,6 @@ class CategoryListView(ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-    def get_queryset(self):
-        title = self.request.query_params.get('title')
-        if title: self.queryset = self.queryset.filter(title__icontains=title)
-        super().get_queryset()
-
 
 class CategoryItemView(RetrieveUpdateAPIView, DestroyAPIView):
     queryset = Category.objects.all()
@@ -36,11 +31,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.core.paginator import Paginator, EmptyPage
+
+
+def paginator(request, queryset):
+        perpage = request.query_params.get('perpage', default=5)
+        perpage = 5 if perpage > 5 else perpage
+
+        page = request.query_params.get('page', default=1)
+
+        paginator = Paginator(queryset, per_page=perpage)
+        try:
+            queryset = paginator.page(number=page)
+        except EmptyPage:
+            queryset = []
+        finally:
+            return queryset
+
 
 @api_view(['GET', 'POST'])
 def menuitem_list(request):
     if request.method == 'GET':
         queryset = MenuItem.objects.select_related('category').all()
+
+        queryset = paginator(request, queryset)
 
         title = request.query_params.get('title')
         if title: queryset = queryset.filter(title=title)
@@ -55,15 +69,13 @@ def menuitem_list(request):
         if search: queryset = queryset.filter(title__icontains=search)
 
         ordering = request.query_params.get('ordering')
-        if ordering:
-            ordering = ordering.split(',')
-            queryset = queryset.order_by(*ordering)
+        if ordering: queryset = queryset.order_by(*ordering.split(','))
 
         serialized_data = MenuItemSerializer(queryset, many=True, context={'request': request})
         return Response(serialized_data.data, status=status.HTTP_200_OK)
 
     if request.method == 'POST':
-        serialized_data = MenuItemSerializer(data=request.data)
+        serialized_data = MenuItemSerializer(data=request.data, context={'request': request})
         serialized_data.is_valid(raise_exception=True)
         serialized_data.save()
         return Response(serialized_data.data, status=status.HTTP_201_CREATED)
