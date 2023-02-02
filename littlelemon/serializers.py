3c50a91cb289f2fa2732221f34d5a650eae471dd
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-02 15:28:04 -0400

    Add RatingSerializer class

--- littlelemon/serializers.py
+++ littlelemon/serializers.py
@@ -1,10 +1,12 @@
-from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User 
 
-from .models import MenuItem, Category
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 
 from decimal import Decimal
 import bleach
+
+from .models import MenuItem, Category, Rating
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -38,3 +40,24 @@ class MenuItemSerializer(serializers.ModelSerializer):
         if attrs['price'] < 2: raise serializers.ValidationError('The price should be greater or equal than 2')
         if attrs['inventory'] < 0: raise serializers.ValidationError('The number of items in inventoy should be greater than 0')
         return super().validate(attrs)
+
+ 
+ 
+class RatingSerializer(serializers.ModelSerializer): 
+    user = serializers.PrimaryKeyRelatedField( 
+        queryset = User.objects.all(), 
+        default = serializers.CurrentUserDefault() 
+    ) 
+
+    class Meta:
+        model = Rating
+        fields = ['user', 'menuitem_id', 'rating']
+        validators = [
+            UniqueTogetherValidator(
+                queryset = Rating.objects.all(),
+                fields = ['user', 'menuitem_id', 'rating']
+            )
+        ]
+        extra_kwargs = {
+            'rating': {'max_value': 5, 'min_value': 0},
+        }
