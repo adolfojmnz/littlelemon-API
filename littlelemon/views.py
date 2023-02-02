0658bd11b80a37a6d1e30283ac1e1e22ab16a0e5
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-02 08:55:27 -0400

    Delete throttle implementation

--- littlelemon/views.py
+++ littlelemon/views.py
@@ -1,33 +1,28 @@
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import MenuItemSerializer, CategorySerializer
-from .throttle import UserThrottleMixin, AnonThrottleMixin
 from .models import MenuItem, Category
 
 
-class MenuItemsViewSet(UserThrottleMixin, AnonThrottleMixin, ModelViewSet):
-    throttle_classes = []
+class MenuItemsViewSet(ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
     ordering_fields = ['title', 'price', 'inventory']
     search_fields = ['title', 'category__title']
 
-    def get_throttles(self):
-        return super().get_throttles()
 
-
-class MenuItemViewSet(UserThrottleMixin, AnonThrottleMixin, ModelViewSet):
+class MenuItemViewSet(ModelViewSet):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
 
-class CategoryItemsViewSet(UserThrottleMixin, AnonThrottleMixin, ModelViewSet):
+class CategoryItemsViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     ordering_fields = ['title']
     search_fields = ['title']
 
 
-class CategoryItemViewSet(UserThrottleMixin, AnonThrottleMixin, ModelViewSet):
+class CategoryItemViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
