b2dd2efbf8585f5720576e8fabaff064e40b4401
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-03 09:33:58 -0400

    Add serializers for littlelemon.models models

---
+++ api/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+
+from littlelemon.models import MenuItem, Category, Cart, Order, OrderItem
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = serializers.HyperlinkedIdentityField(view_name='category-detail')
+
+    class Meta:
+        model = MenuItem
+        fields = ['title', 'price', 'featured', 'category_id']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ['title', 'slug']
+
+
+class CartSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Cart
+        fields = ['user', 'menuitem', 'quantity', 'unit_price', 'price']
+    
+
+class OrderSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Order
+        fields = ['user', 'delivery_crew', 'status', 'total', 'date']
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = OrderItem
+        fields = ['order', 'menuitem', 'quantity', 'unit_price', 'price']
