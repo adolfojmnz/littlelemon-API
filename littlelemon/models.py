1c690d75acb32dc225f4eaf8b5a1acd66925738f
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-03 07:40:10 -0400

    Add Model classes and generate migrations

--- littlelemon/models.py
+++ littlelemon/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
+    category = models.ForeignKey('littlelemon.Category', on_delete=models.PROTECT)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255)
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ['menuitem', 'user']
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE, 
+        related_name = 'delivery_crew',
+        null = True,
+    )
+    status = models.BooleanField(db_index=True, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField(auto_now_add=True, db_index=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ['order', 'menuitem']
