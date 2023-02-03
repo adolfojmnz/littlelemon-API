c47e337d5b4610f540e6ba35d4ec0d40f21046d2
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-03 09:33:12 -0400

    Register models

--- littlelemon/admin.py
+++ littlelemon/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import (
+    MenuItem, Category, Cart, Order, OrderItem,
+)
+
+admin.site.register(MenuItem)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(Order)
+admin.site.register(OrderItem)
