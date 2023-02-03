f8545cf2663e0293f4b5eece64273a3868f1afb2
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-03 09:35:06 -0400

    Add serialized views for littlelemon.models models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from littlelemon.models import (
+    MenuItem, Category, Cart, Order, OrderItem,
+)
+
+from .serializers import (
+    MenuItemSerializer,
+    CategorySerializer,
+    CartSerializer,
+    OrderSerializer,
+    OrderItemSerializer,
+)
+
+
+class MenuItemListViewSet(ModelViewSet):
+    model = MenuItem
+    queryset = model.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class MenuItemDetailViewSet(ModelViewSet):
+    model = MenuItem
+    serializer_class = MenuItemSerializer
+
+
+class CategoryListViewSet(ModelViewSet):
+    model = Category
+    queryset = model.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailViewSet(ModelViewSet):
+    model = Category
+    serializer_class = CategorySerializer
+
+
+class CartListViewSet(ModelViewSet):
+    model = Cart
+    queryset = model.objects.all()
+    serializer_class = CartSerializer
+
+
+class CartDetailViewSet(ModelViewSet):
+    model = Cart
+    serializer_class = CartSerializer
+
+
+class OrderListViewSet(ModelViewSet):
+    model = Order
+    queryset = model.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderDetailViewSet(ModelViewSet):
+    model = Order
+    serializer_class = OrderSerializer
+
+
+class OrderItemListViewSet(ModelViewSet):
+    model = OrderItem
+    queryset = model.objects.all()
+    serializer_class = OrderItemSerializer
+
+
+class OrderItemDetailViewSet(ModelViewSet):
+    model = OrderItem
+    serializer_class = OrderItemSerializer
