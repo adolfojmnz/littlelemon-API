16b338108b1045bd0e9dbb04f16cab9e9e15f3c3
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-06 20:32:06 -0400

    Update and Create Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,40 +1,66 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User, Group
 
-from littlelemon.models import MenuItem, Category, Cart, Order, OrderItem
+from littlelemon.models import (
+    MenuItem, Category, Cart, Order, OrderItem, Purchase,
+)
 
 
-class MenuItemSerializer(serializers.ModelSerializer):
-    category = serializers.HyperlinkedIdentityField(view_name='category-detail', read_only=True)
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ['id', 'name']
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+    
+
+class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
     category_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = MenuItem
-        fields = ['title', 'price', 'featured', 'category', 'category_id']
+        fields = ['id', 'title', 'price', 'featured', 'category', 'category_id']
+        read_only_fields = ['category']
 
 
 class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ['title', 'slug']
-
-
-class CartSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Cart
-        fields = ['user', 'menuitem', 'quantity', 'unit_price', 'price']
-    
-
-class OrderSerializer(serializers.ModelSerializer):
-    
-    class Meta:
-        model = Order
-        fields = ['user', 'delivery_crew', 'status', 'total', 'date']
+        fields = ['id', 'title', 'slug']
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = OrderItem
-        fields = ['order', 'menuitem', 'quantity', 'unit_price', 'price']
+        fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
+        read_only_fields = ['user', 'unit_price', 'price']
+
+
+class CartSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'orderitems']
+    
+
+class PurchaseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Purchase
+        fields = ['user', 'orderitems', 'date']
+        read_only = ['user', 'date']
+        
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'purchase', 'delivery_crew', 'delivered', 'total', 'date']
