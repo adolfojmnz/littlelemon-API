e3afbe4e5153ed1aa363a760609de3346f9c4b8d
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-07 10:26:25 -0400

    Delete unrequired file

--- api/helpers.py
+++
@@ -1,7 +0,0 @@
-def user_is_requested_user(request):
-
-    current_user_pk = request.user.pk
-    requested_user_pk = request.parser_context['kwargs'].get('pk')
-    if current_user_pk == requested_user_pk and requested_user_pk is not None:
-        return True
-    return False
