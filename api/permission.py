dab9964c157be125ce23dced259a8da6aff9a720
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-06 20:25:24 -0400

    Add IsCustomer permission class

--- api/permission.py
+++ api/permission.py
@@ -24,15 +24,5 @@ class IsDeliveryCrew(PermissionBaseMixin):
     group = 'Delivery Crew'
 
 
-class IsCustomer(BasePermission):
-    customer_allowed_methods = ['GET']
-    requires_customer_authentication = True
-
-    def has_permission(self, request, view):
-        if not request.method in self.customer_allowed_methods:
-            return False
-        if not self.requires_customer_authentication:
-            return True
-        if not bool(request.user and request.user.is_authenticated):
-            return False
-        return True
+class IsCustomer(PermissionBaseMixin):
+    group = 'Customer'
