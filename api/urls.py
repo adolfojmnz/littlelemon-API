5210fca7eb2314a8f46834d7b127a12a7654a4ae
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-06 20:32:42 -0400

    Update and Create URLs

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
 from .views import (
+    UserDetailView,
+    GroupListViewSet, GroupDetailView,
+    SysAdminListViewSet, SysAdminDetailViewSet,
+    ManagerListViewSet, ManagerDetailViewSet,
+    DeliveryCrewListViewSet, DeliveryCrewDetailViewSet,
+    CustomerListViewSet, CustomerDetailViewSet,
     MenuItemListViewSet, MenuItemDetailViewSet,
     CategoryListViewSet, CategoryDetailViewSet,
-    CartListViewSet, CartDetailViewSet,
-    OrderListViewSet, OrderDetailViewSet,
-    OrderItemListViewSet, OrderItemDetailViewSet,
+    OrderItemListViewSet,
+    OrderItemDetailViewSet,
+    CartViewSet,
+    OrderListViewSet,
+    OrderDetailViewSet,
+    PurchaseListView,
+    PurchaseDetailView,
 )
 
 LIST = {'get': 'list', 'post': 'create'}
@@ -13,18 +23,33 @@ DETAIL = {'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete
 
 
 urlpatterns = [
+    path('users/<int:pk>', UserDetailView.as_view(), name='user-detail'),
+
+    path('groups', GroupListViewSet.as_view(LIST)),
+    path('groups/<int:pk>', GroupDetailView.as_view(DETAIL)),
+    path('groups/admins', SysAdminListViewSet.as_view(LIST)),
+    path('groups/admins/<int:pk>', SysAdminDetailViewSet.as_view(DETAIL)),
+    path('groups/managers', ManagerListViewSet.as_view()),
+    path('groups/managers/<int:pk>', ManagerDetailViewSet.as_view(DETAIL)),
+    path('groups/delivery-crew', DeliveryCrewListViewSet.as_view()),
+    path('groups/delivery-crew/<int:pk>', DeliveryCrewDetailViewSet.as_view(DETAIL)),
+    path('groups/customers', CustomerListViewSet.as_view(LIST)),
+    path('groups/customers/<int:pk>', CustomerDetailViewSet.as_view(DETAIL)),
+
     path('menu-items', MenuItemListViewSet.as_view(LIST)),
-    path('menu-items/<int:pk>', MenuItemDetailViewSet.as_view(DETAIL)),
+    path('menu-items/<int:pk>', MenuItemDetailViewSet.as_view(DETAIL), name='menuitem-detail'),
 
     path('categories', CategoryListViewSet.as_view(LIST)),
     path('categories/<int:pk>', CategoryDetailViewSet.as_view(DETAIL), name='category-detail'),
 
-    path('carts', CartListViewSet.as_view(LIST)),
-    path('carts/<int:pk>', CartDetailViewSet.as_view(DETAIL)),
+    path('cart', CartViewSet.as_view()),
 
-    path('orders', OrderListViewSet.as_view(LIST)),
-    path('orders/<int:pk>', CartDetailViewSet.as_view(DETAIL)),
+    path('order-items', OrderItemListViewSet.as_view()),
+    path('order-items/<int:pk>', OrderItemDetailViewSet.as_view(), name='orderitem-detail'),
 
-    path('order-items', OrderItemListViewSet.as_view(LIST)),
-    path('order-items/<int:pk>', OrderItemDetailViewSet.as_view(DETAIL)),
+    path('purchases', PurchaseListView().as_view()),
+    path('purchases/<int:pk>', PurchaseDetailView().as_view(), name='purchase-detail'),
+
+    path('orders', OrderListViewSet.as_view()),
+    path('orders/<int:pk>', OrderDetailViewSet.as_view(DETAIL)),
 ]
