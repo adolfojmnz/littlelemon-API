9f464548e9eff128969679890032a6197d86ac0f
Adolfo Jimenez <adolfoj@protonmail.com>
2023-02-06 20:28:36 -0400

    Add urls updates

--- config/urls.py
+++ config/urls.py
@@ -9,11 +9,17 @@ from rest_framework_simplejwt.views import (
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # API
     path('api/', include('api.urls')),
+
+    # Djoser
     path('api/', include('djoser.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/', include('djoser.urls.authtoken')),
+
+    # JWT
+    path('api/token/login/', TokenObtainPairView.as_view(),  name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-
-    path('admin/', admin.site.urls),
 ]
